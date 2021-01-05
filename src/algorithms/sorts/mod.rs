d126f2f8c0c7dabaa8fd0542e084a6d397b6c15b
Egor <bronnikov.40@mail.ru>
2021-01-05 20:15:43 +0300

    :crab: Added Shell sorting

--- src/algorithms/sorts/mod.rs
+++ src/algorithms/sorts/mod.rs
@@ -3,6 +3,7 @@ mod insertion_sort;
 mod merge_sort;
 mod quick_sort;
 mod selection_sort;
+mod shell_sort;
 
 
 pub use self::bubble_sort::bubble_sort;
@@ -10,3 +11,4 @@ pub use self::insertion_sort::insertion_sort;
 pub use self::merge_sort::merge_sort;
 pub use self::quick_sort::quick_sort;
 pub use self::selection_sort::selection_sort;
+pub use self::shell_sort::shell_sort;
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ mod constructions;
 fn main() {
 	let vec: Vec<isize> = constructions::random_list(10, 1, 9);
 	println!("{:?}", vec);
-	let sorted: Vec<isize> = algorithms::sorts::quick_sort(vec);
+	let sorted: Vec<isize> = algorithms::sorts::shell_sort(vec);
 	println!("{:?}", sorted);
-	println!("{}", algorithms::searches::binary_search(sorted, 1));
 }
